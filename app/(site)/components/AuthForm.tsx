68ad5190442de731a424e11a11af3a7eb3683e92
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-06-25 16:59:32 +0000

    Logic for Auth

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,10 +1,48 @@
-'use clinet';
-
+'use client';
+import {useCallback,useState} from "react";
+import {FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+type Variant = 'LOGIN' | 'REGISTER'
 const AuthForm= () =>{
+    const [variant,setVariant] = useState<Variant>('LOGIN');
+    const [isLoading,setisLoading] = useState(false)
+    const toggleVariant = useCallback(()=>{
+            if(variant ==='LOGIN'){
+                setVariant('REGISTER')
+            }else{
+                setVariant('LOGIN')
+            }
+    },[variant]);
+
+    const {register,handleSubmit,formState:{
+         errors
+    }} = useForm<FieldValues>({
+        defaultValues:{
+            name: '',
+            email: '',
+            password: ''
+        }
+    })
+
+    const onSubmit: SubmitHandler<FieldValues>=(data) =>{
+        setisLoading(true); 
+        if(variant === 'REGISTER'){
+            //Axios Register
+        }else{
+            //NextAuth  SignIn
+        }
+        }
+     
     return (
-        <div>Auth Form</div>
+        <div className="mt-8 sm:max-auto sm:w-full sm:max-w-md">
+
+                <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+
+                </div>
+        </div>
+
+
     );
 
 }
 
-export default AuthForm
+export default AuthForm 
